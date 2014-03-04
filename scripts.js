8522e21c8a82d769da6166e66a013f6fb0bb1ea0
HarryStevens <harryjosephstevens@gmail.com>
2014-03-04 11:46:09 -0500

    Custom scripts are working and chart is formatted propery

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 /**
- * @author
+ * @author Harry Stevens
  */
 
 //1. Document ready calls pageLoaded function
@@ -15,10 +15,73 @@ function pageLoaded() {
 
 //3. googleLoaded function loads the data and calls the dataLoaded function
 function googleLoaded() {
-	$.get("URL", dataLoaded, "json");
+	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1_g7L00qKd8I9rNtTrWgS1iHzkh8SmTYy8oS_VC99&key=AIzaSyB-QJux9WIJmey5IJYzPImNzg-xP1gpvU8", dataLoaded, "json");
 }
 
 //4. dataLoaded function formats the data, runs it throw the Google Visualizaiton library, and displays it
-function dataLoaded(){
+function dataLoaded(UNEMP) {
+
+	//This entire section until the end of the for loop is because I have to change the date string in my data to an actual date
+	var dataRows = UNEMP.rows;
+	//empty array to fill with data for Google
+	var dataArray = [];
 	
+
+	for (var i = 0; i < dataRows.length; i++) {
+		var currRow = dataRows[i];
+
+		//Moment.js will convert the date string to an actual date
+		var currDate = currRow[0];
+		var momentDate = moment(currDate);
+		var finalDate = momentDate._d;
+
+		//Grabbing the value number
+		var currValue = currRow[1];
+
+		//Creating my array with the date and value number for the Google Viz
+		var currArray = [finalDate, currValue];
+		dataArray.push(currArray);
+	}
+
+	//This code is from the Google Visualization DataTable documentation. It will create a table of data that I can display however I want.
+	var data = new google.visualization.DataTable();
+	data.addColumn('date', 'Date');
+	data.addColumn('number', 'Unemployment');
+	data.addRows(dataArray);
+
+	//This section draws the chart
+	var options = {
+		title : 'Select a date range to zoom in. Right click to zoom out.',
+		legend : {
+			position : 'none'
+		},
+		backgroundColor : {
+			stroke : '#000',
+			strokeWidth : 4
+		},
+		curveType : 'function',
+		explorer : {
+			actions : ['dragToZoom', 'rightClickToReset'],
+			axis : 'horizontal',
+			maxZoomIn : .01
+		},
+		height : 520,
+		chartArea : {
+			height : 380,
+			width : 1000
+		},
+		hAxis : {
+			title : 'Date'
+		},
+		vAxis : {
+			title : 'Percent unemployed',
+			ticks : [0, 2, 4, 6, 8, 10, 12],
+			format : '#'
+		},
+		selectionMode : 'multiple'
+	};
+
+	var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+
 }
