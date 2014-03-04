05b1f7ac186f75f26650c8571c24b72962fab372
HarryStevens <harryjosephstevens@gmail.com>
2014-03-04 10:43:36 -0500

    Preparing to load data from fusion table

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
 /**
  * @author
  */
+
+//1. Document ready calls pageLoaded function
+$(document).ready(pageLoaded);
+
+//2. pageLoaded function loads the Google Visualization library and calls googleLoaded function
+function pageLoaded() {
+	google.load("visualization", "1", {
+		packages : ["corechart"],
+		callback : googleLoaded
+	});
+}
+
+//3. googleLoaded function loads the data and calls the dataLoaded function
+function googleLoaded() {
+	$.get("URL", dataLoaded, "json");
+}
+
+//4. dataLoaded function formats the data, runs it throw the Google Visualizaiton library, and displays it
+function dataLoaded(){
+	
+}
